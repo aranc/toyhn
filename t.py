8f1fee2628c89b7b88d9bb6a5a21356ad403c281
Aran Carmon <aran.carmon@gmail.com>
2019-12-02 17:15:07 +0200

    .

--- t.py
+++ t.py
@@ -110,13 +110,16 @@ def train(data_generator):
 
         if True:
             grad_list = []
-            for p in filter(lambda p: p.requires_grad, g.parameters()):
+            #or p in filter(lambda p: p.requires_grad, g.parameters()):
+            for p in g.parameters():
                 grad_list.append(p.grad.view(-1))
                 break
             grad_list = torch.cat(grad_list, 0)
             all_grads = []
             all_grads.append(grad_list.detach())
             all_grads = torch.stack(all_grads, 0)
+            print(new_weights.shape, all_grads.shape)
+            exit()
             new_weights.backward(all_grads)
 
         optimizer.step()
