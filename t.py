1f4e96ddf40048dc8a3051889a1c391376102065
Aran Carmon <aran.carmon@gmail.com>
2019-12-02 17:29:44 +0200

    .

--- t.py
+++ t.py
@@ -21,8 +21,8 @@ def gen_data_entry(op):
 class G(torch.nn.Module):
     def __init__(self):
         super(G, self).__init__()
-        self.net = torch.nn.Linear(2, 2, bias=False)
-        self.net2 = torch.nn.Linear(2, 2, bias=False)
+        self.net = torch.nn.Linear(2, 2, bias=True)
+        self.net2 = torch.nn.Linear(2, 2, bias=True)
         self.net3 = torch.nn.Linear(2, 1, bias=False)
 
     def load_weights(self, new_weights):
