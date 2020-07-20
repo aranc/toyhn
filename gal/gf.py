67fba6b422f91d73df6c0a01543bb1fd1699810c
Aran Carmon <aran.carmon@gmail.com>
2020-07-20 14:02:30 +0300

    .

--- gal/gf.py
+++ gal/gf.py
@@ -4,35 +4,6 @@ import numpy as np
 import torch
 import torch.nn as nn
 
-class Network(torch.nn.Module):
-    def __init__(self, args=None):
-        super(Network, self).__init__()
-        self.linear = nn.Linear(2, 1)
-
-    def forward(self, op, x):
-        xy = torch.cat((op, x), dim=-1)
-        return self.linear(xy)
-
-class HyperNetwork1(torch.nn.Module):
-    def __init__(self, args=None):
-        super(HyperNetwork1, self).__init__()
-        self.get_w = nn.Linear(1, 1)
-        self.get_b = nn.Linear(1, 1)
-
-    def forward(self, op, x):
-        return self.get_w(op) * x + self.get_b(op)
-
-class HyperNetwork2(torch.nn.Module):
-    def __init__(self, args=None):
-        super(HyperNetwork2, self).__init__()
-        self.get_w = nn.Linear(1, 1)
-        self.get_b = nn.Linear(1, 1)
-
-    def forward(self, op, x):
-        weight = self.get_w(op)
-        bias = self.get_b(op)
-        return torch.nn.functional.linear(x, weight, bias)
-
 class HyperNetwork3(torch.nn.Module):
     def __init__(self, args=None):
         super(HyperNetwork3, self).__init__()
@@ -56,16 +27,6 @@ class HyperNetwork3(torch.nn.Module):
         x = torch.nn.functional.linear(x, weight2, bias2)
         return x
 
-if sys.argv[1] == "Network":
-    net = Network()
-elif sys.argv[1] == "HyperNetwork1":
-    net = HyperNetwork1()
-elif sys.argv[1] == "HyperNetwork2":
-    net = HyperNetwork2()
-elif sys.argv[1] == "HyperNetwork3":
-    net = HyperNetwork2()
-else:
-    assert False
 
 learn_rate = 1e-3
 optimizer = torch.optim.Adam(net.parameters(), lr=learn_rate)
