458aaa2635e2aead9fa6db0a3117830990a0b06d
Aran Carmon <aran.carmon@gmail.com>
2020-07-20 15:38:20 +0300

    .

---
+++ gal/lstms/pg.py
@@ -0,0 +1,37 @@
+import sys
+import random
+import numpy as np
+import torch
+import torch.nn as nn
+
+class Network(torch.nn.Module):
+    def __init__(self, args=None):
+        super(Network, self).__init__()
+        self.lstm = nn.LSTM(1, 1, batch_first=True)
+
+    def forward(self, x):
+        return self.lstm(x)[1][0]
+
+net = Network()
+learn_rate = 1e-3
+optimizer = torch.optim.Adam(net.parameters(), lr=learn_rate)
+
+seq_len = 3
+
+while True:
+    optimizer.zero_grad()
+
+    x = [random.random() for _ in range(seq_len)]
+
+    y = x[-1]
+
+    x = torch.FloatTensor([x]).unsqueeze(0)
+    y = torch.FloatTensor([y]).unsqueeze(0)
+    
+    prediction = net(x)
+    loss = (y - prediction) ** 2
+    print(loss.item())
+
+    loss.backward()
+    optimizer.step()
+
